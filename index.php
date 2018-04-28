eaef81f3f30065ece16018a807b8501d87a7e6c0
Klutzershy <33833441+Klutzershy@users.noreply.github.com>
2018-04-27 20:18:27 -0400

    Added Essentials
    Added in footer, header, and improved index files

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!--Bootstrap link -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<title>Bradley Brown's Portfolio</title>
+</head>
+<body>
+  <div class="container-fluid">
+    <?php
+      include 'header.php';
+    ?>
+
+
+    <?php
+      include 'footer.php';
+      include 'boostrap_js.php';
+    ?>
+  </div>
+</body>
+</html>
